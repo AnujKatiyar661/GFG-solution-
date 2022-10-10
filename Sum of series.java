f41e28a529b3a52f5d2e4d30ae2a93497aab1839
Abhishek Ranjan <111702473+Abhiranjan0@users.noreply.github.com>
2022-10-10 21:09:24 +0530

    Update Sum of series.java

--- Sum of series.java
+++ Sum of series.java
@@ -3,10 +3,7 @@
 
 class Solution {
    
-    long seriesSum(int n) {
-        // code here
-       //   long sum=(long)n*(n+1)/2;
-    // we can do it by for loop 
+    long seriesSum(int n) {  
     long sum=0L;
     for(int i=1;i<=n;i++)
     {
